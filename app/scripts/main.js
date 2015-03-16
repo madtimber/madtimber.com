fb152ace6d71d7ae519681f9e31da711797dbf3f
Todd Baker <toddlbaker@gmail.com>
2015-03-16 18:09:48 -0400

    cleaned up jshint errors.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,17 @@
 /* jshint devel:true */
+'use strict';
 
 $(document).ready(function() {
 
   $('form').on('submit', function(e) {
     var form = $(this);
     $.post(form.attr('action'), {
-      'name': form.find("#name").val(),
-      'email': form.find("#email").val(),
-      'message': form.find("#message").val(),
-    }).done(function(res) {
-      form.find("input[type!=submit], textarea").val("");
-      form.find("button").text("Sent!").addClass("messageSent");
+      'name': form.find('#name').val(),
+      'email': form.find('#email').val(),
+      'message': form.find('#message').val(),
+    }).done(function() {
+      form.find('input[type!=submit], textarea').val('');
+      form.find('button').text('Sent!').addClass('messageSent');
     }).error(function(err) {
       alert(err);
     });
